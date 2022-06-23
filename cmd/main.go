6e799b1105ccd9c50a493eeaebda9ac0ad4c2a9d
kcholombytko <funnygungan@gmail.com>
2022-06-23 23:46:19 +0300

    implement main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
 
+	"github.com/cr1ng3T34m1337/arch-lab4-event-loop/pkg/engine"
+	"github.com/cr1ng3T34m1337/arch-lab4-event-loop/pkg/parser"
+)
+
+func main() {
+	eventLoop := new(engine.EventLoop)
+	eventLoop.Start()
+	if input, err := os.Open("instructions.txt"); err == nil {
+		defer input.Close()
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			commandLine := scanner.Text()
+			cmds := parser.Parse(commandLine)
+			for _, cmd := range cmds {
+				eventLoop.Post(cmd)
+			}
+		}
+	} else {
+		log.Fatal(err)
+	}
+	eventLoop.AwaitFinish()
 }
